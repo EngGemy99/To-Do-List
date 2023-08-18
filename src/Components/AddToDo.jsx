bf2f2ae08cdce2152f8c4d8a273ec5f63f077d2b
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-18 22:29:15 +0300

    some edit in component

--- src/Components/AddToDo.jsx
+++ src/Components/AddToDo.jsx
@@ -1,24 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { memo, useContext, useState, useCallback, useRef } from "react";
 import Tasks from "../Context/ToDo";
 import { ToDoTask } from "../config/axiosConfig";
 import { toast } from "react-toastify";
 
-function AddToDo() {
+const AddToDo = memo(() => {
   const { setTasks } = useContext(Tasks);
-  const [task, setTask] = useState("");
-  const [description, setDescription] = useState("");
+  const taskRef = useRef(null);
+  const descriptionRef = useRef(null);
   const [loading, setLoading] = useState(false);
 
-  const handleAdd = async () => {
+  const handleAdd = useCallback(async () => {
+    const task = taskRef.current.value;
+    const description = descriptionRef.current.value;
+
     if (!task.trim() || !description.trim()) {
-      toast.error(" task and description are required", {
+      toast.error("Task and description are required", {
         position: "top-right",
         autoClose: 1000,
       });
       return;
     }
+    setLoading(true);
     try {
-      setLoading(true);
       const { data } = await ToDoTask.post("toDos", {
         task,
         description,
@@ -31,28 +34,26 @@ function AddToDo() {
     } catch (error) {
       console.log(error);
     } finally {
-      setTask("");
-      setDescription("");
+      taskRef.current.value = "";
+      descriptionRef.current.value = "";
       setLoading(false);
     }
-  };
+  }, [setTasks]);
 
   return (
     <div className="flex flex-col justify-center items-center w-full mb-10">
       <input
+        ref={taskRef}
         className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="task"
         type="text"
         placeholder="Task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
       />
       <textarea
+        ref={descriptionRef}
         className="shadow appearance-none border rounded w-1/2 h-20 mt-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="description"
         placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
       />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
@@ -60,10 +61,10 @@ function AddToDo() {
         onClick={handleAdd}
         disabled={loading}
       >
-        {loading ? "Adding..." : "Add task"}
+        {loading ? "Adding..." : "Add Task"}
       </button>
     </div>
   );
-}
+});
 
 export default AddToDo;
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,29 +1,45 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SectionList from "./SectionList";
 import Tasks from "../Context/ToDo";
 
 function List() {
   const { tasks } = useContext(Tasks);
-  const [filteredTasks, setFilteredTasks] = useState([]);
-  const [toDos, setToDos] = useState([]);
-  const [inProcess, setInProcess] = useState([]);
-  const [completed, setCompleted] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+  const [taskTypes, setTaskTypes] = useState({
+    toDos: [],
+    inProcess: [],
+    completed: [],
+  });
+
   useEffect(() => {
     setFilteredTasks([...tasks]);
   }, [tasks]);
+
   useEffect(() => {
-    let toDosTasks = filteredTasks.filter((task) => task.state === "todo");
-    let inProcessTasks = filteredTasks.filter(
-      (task) => task.state === "inprogress"
-    );
-    let completedTasks = filteredTasks.filter(
-      (task) => task.state === "complete"
-    );
-    setToDos(toDosTasks);
-    setInProcess(inProcessTasks);
-    setCompleted(completedTasks);
+    setTaskTypes({
+      toDos: filteredTasks.filter((task) => task.state === "todo"),
+      inProcess: filteredTasks.filter((task) => task.state === "inprogress"),
+      completed: filteredTasks.filter((task) => task.state === "complete"),
+    });
   }, [filteredTasks]);
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+
+    if (!searchValue.trim()) {
+      setFilteredTasks(tasks);
+    } else {
+      const lowerCaseSearch = searchValue.toLowerCase();
+      setFilteredTasks(
+        tasks.filter((task) =>
+          task.task.toLowerCase().includes(lowerCaseSearch)
+        )
+      );
+    }
+  };
+
   const states = ["todo", "inprogress", "complete"];
+
   return (
     <div className="bg-slate-500 w-3/4 p-4">
       <input
@@ -31,25 +47,16 @@ function List() {
         id="search"
         type="text"
         placeholder="Search"
-        onChange={(e) => {
-          if (e.target.value !== "") {
-            const filteredData = tasks.filter((item) =>
-              item.task.toLowerCase().includes(e.target.value.toLowerCase())
-            );
-            setFilteredTasks(filteredData);
-          } else {
-            setFilteredTasks(tasks);
-          }
-        }}
+        onChange={handleSearch}
       />
       <div className="flex flex-col md:flex-row justify-between items-baseline md:items-baseline  w-full mt-8 px-4 md:px-0 gap-3">
         {states.map((state) => (
           <SectionList
             key={state}
             state={state}
-            toDos={toDos}
-            inProcess={inProcess}
-            completed={completed}
+            toDos={taskTypes.toDos}
+            inProcess={taskTypes.inProcess}
+            completed={taskTypes.completed}
           />
         ))}
       </div>
